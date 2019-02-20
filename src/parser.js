50dd501e82448766df8e41e24f5586629c67164a
Nan Li <Nan.Li@bcaa.com>
2019-02-19 16:59:07 -0800

    click

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,10 @@
 const tmp = require('./templates');
 
 module.exports = function (source) {
-  return tmp.handler.replace('{0}', source);
+  const [i, o] = source.split('=>');
+  const ev = 'click';
+  const name = 'add';
+  return `
+  window.onload = () => document.getElementsByName('${name}')[0].addEventListener('${ev}', () => alert('abc'));
+`;
 }
